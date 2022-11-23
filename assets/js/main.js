ed47ae6a72027ee34a691c48e575a9d2e33e67d7
sellamiTSP <99595682+sellamiTSP@users.noreply.github.com>
2022-11-23 16:26:37 +0100

    add coopis2023

--- assets/js/main.js
+++ assets/js/main.js
@@ -54,7 +54,7 @@ $(document).ready(function() {
 	
 	/* ======= Countdown ========= */
 	// set the date we're counting down to
-    var target_date = new Date("Oct 4, 2022").getTime();
+    var target_date = new Date("Oct 1, 2023").getTime();
      
     // variables for time units
     var days, hours, minutes, seconds;
